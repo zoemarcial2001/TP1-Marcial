3c3a5e8f40be88faec20c3b5df98494599ef669a
Zoe Marcial <Zoe Marcial@LAPTOP-QPF45C5K>
2021-04-07 21:31:52 -0300

    Trabajo en clase ProvinciaController

--- src/main/java/ar/edu/unju/edm/tp1pto1/controller/ProvinciaController.java
+++ src/main/java/ar/edu/unju/edm/tp1pto1/controller/ProvinciaController.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.edm.tp1pto1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+
 public class ProvinciaController {
+	
+	@GetMapping ({"/provincia"})
+	
+	public String cargaProvincia() {
+		return "provincia";
+	}
+	
 
 }
