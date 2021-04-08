18231f926215f5ec9f25bd1ac59893890da784c1
Zoe Marcial <Zoe Marcial@LAPTOP-QPF45C5K>
2021-04-07 21:17:26 -0300

    Creacion html

--- src/main/java/ar/unju/edu/tp1/model/Provincia.java
+++ src/main/java/ar/unju/edu/tp1/model/Provincia.java
@@ -1,29 +1,33 @@
 package ar.unju.edu.tp1.model;
 
 public class Provincia {
-	int codigo;
-	String nombre;
+	private int codigo;
+	private String nombre;
+	
+	public Provincia() {
+		
+	}
 	
 	
-	
-	
+	public Provincia(int codigo, String nombre) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
+
 
 	public int getCodigo() {
 		return codigo;
 	}
-
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -32,7 +36,6 @@ public class Provincia {
 		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -51,12 +54,9 @@ public class Provincia {
 			return false;
 		return true;
 	}
-
 	@Override
 	public String toString() {
 		return "Provincia [codigo=" + codigo + ", nombre=" + nombre + "]";
 	}
-	
-	
 
 }
